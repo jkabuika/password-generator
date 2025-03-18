cad01f19810f3203189b5d7feced73eb5d925355
jkabuika <jkabuika1319@gmail.com>
2025-03-18 01:46:48 -0400

    Added password generator

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
+
 const passwordDisplay = document.querySelector('.password');
 const copyBtn = document.querySelector('.copy-btn')
 const generateBtn = document.querySelector('.generate-btn')
 const largeCopyBtn = document.querySelector('.large-copy-btn')
 const lengthInput = document.querySelector(".length-option-container input");
 const checkboxes = document.querySelectorAll(".character-type input");
-}
+
+const charSets = {
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  numbers: "0123456789",
+  symbols: "!@#$%^&*?"
+};
+
